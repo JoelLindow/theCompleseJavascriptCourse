0cd4aea22ad54ba29401608afacb09e6abf5efae
JoelLindow <joellindow@gmail.com>
2017-11-28 11:40:21 -0700

    set display property to NOTHING for the dice. Getting ready to code it.

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -20,9 +20,15 @@ dice = Math.floor(Math.random() * 6) + 1;
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // we don't use textContent here becasue we can only do text. not html. We need the innerHtml method
 document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// this is a setter becasue it sets a value
 
 
+var x = document.querySelector('#score-0').textContent;
+//this is a getter becasue it gets a value
+console.log(x);
 
+document.querySelector('.dice').style.display = 'none';
+// if you inspect the css it will show that the display property for the .dice class was set to 'none'
 
 
 //
