c500aa63ab07cff2390b4ff1ae41d6a3ee00db0c
JoelLindow <joellindow@gmail.com>
2017-11-28 20:08:44 -0700

    getElement and getElementById

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -9,26 +9,50 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// we don't use textContent here becasue we can only do text. not html. We need the innerHtml method
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-// this is a setter becasue it sets a value
-
-
-var x = document.querySelector('#score-0').textContent;
-//this is a getter becasue it gets a value
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
 // if you inspect the css it will show that the display property for the .dice class was set to 'none'
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+// These will replace any existing HTML test with what we've designated in the code! WOOT! 
 
-//
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // ANONYMOUS FUNCTION - A function that has no name that can not be resued!
+
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png'
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+
+});
+// event listener has 2 arguments. Type of event and function that should be called when event happens
+// a CALLBACK function is a function passed into another function where an event calls it for us
+
+
+
+
+
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// we don't use textContent here becasue we can only do text. not html. We need the innerHtml method
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// this is a setter becasue it sets a value
+//var x = document.querySelector('#score-0').textContent;
+//this is a getter becasue it gets a value
