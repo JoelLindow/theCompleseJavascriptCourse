a935fb378769ecc49633d8014e3175d516581c57
JoelLindow <joellindow@gmail.com>
2017-12-11 10:36:11 -0700

    everything is working! Can reset game. Can win game! When you win the code is blocked for hold and roll

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,51 +8,53 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // ANONYMOUS FUNCTION - A function that has no name that can not be resued!
+  if (gamePlaying) {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-  // 1. Random number
-  var dice = Math.floor(Math.random() * 6) + 1;
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
 
-  // 2. Display the result
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png'
-
-  // 3. Update the round score IF the rolled number was NOT a 1
-  if (dice !== 1) {
-    // Add score
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-    // Remember! We have add, remove and toggle
-  } else {
-    //Next player
-    nextPlayer();
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+      // Add score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      //Next player
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  // Add CURRENT score to GLOBAL score
-  scores[activePlayer] += roundScore;
+  if (gamePlaying) {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
 
+    // Update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-  // Update UI
-  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-  // Check if player won the Game
-  if (scores[activePlayer] >= 10) {
-    document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-  } else {
-
-    // Next player
-    nextPlayer();
+    // Check if player won the Game
+    if (scores[activePlayer] >= 10) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      // Remember! We have add, remove and toggle
+      gamePlaying = false;
+    } else {
+      // Next player
+      nextPlayer();
+    }
   }
 });
 
@@ -63,12 +65,8 @@ function nextPlayer() {
 
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
-
-  // document.querySelector('.player-0-panel').classList.remove('active');
-  // document.querySelector('.player-1-panel').classList.add('active');
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
-
   document.querySelector('.dice').style.display = 'none';
 };
 
@@ -97,8 +95,6 @@ function init() {
 
 // event listener has 2 arguments. Type of event and function that should be called when event happens
 // a CALLBACK function is a function passed into another function where an event calls it for us
-
-
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // we don't use textContent here becasue we can only do text. not html. We need the innerHtml method
